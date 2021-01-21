3358cf628a286b48f9656431a45ee74ceedb6484
MaksymBukshovan <mbukshovan@danavero.com>
2021-01-21 21:54:48 +0200

    Added XCFramework

---
+++ OkCore.xcframework/ios-arm64_i386_x86_64-simulator/OkCore.framework/Headers/OkCore.h
@@ -0,0 +1,18 @@
+//
+//  OkCore.h
+//  OkCore
+//
+//  Created by Maxim on 20.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OkCore.
+FOUNDATION_EXPORT double OkCoreVersionNumber;
+
+//! Project version string for OkCore.
+FOUNDATION_EXPORT const unsigned char OkCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OkCore/PublicHeader.h>
+
+
