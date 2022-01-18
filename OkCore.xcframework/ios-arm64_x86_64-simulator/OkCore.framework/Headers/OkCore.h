8fdac997e66c767363b317efa4fe5f7d29a5e406
LongVo <long.vu@aeris.net>
2022-01-18 14:07:30 +0700

    update spm to v12

--- OkCore.xcframework/ios-arm64_x86_64-simulator/OkCore.framework/Headers/OkCore.h
+++ OkCore.xcframework/ios-arm64_x86_64-simulator/OkCore.framework/Headers/OkCore.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double OkCoreVersionNumber;
 FOUNDATION_EXPORT const unsigned char OkCoreVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <OkCore/PublicHeader.h>
-
-
